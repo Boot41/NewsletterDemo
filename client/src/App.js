61b32206d8187e1c3b0d9b74d3fcd19ce96505db
fayaz41 <fayaz.lnu@think41.com>
2024-10-15 12:30:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Testing TQ CRON JOB
+          Hello Testing TQ CRON 
         </a>
       </header>
     </div>
